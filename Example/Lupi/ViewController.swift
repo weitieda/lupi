da6bfadf745f8e7fb3bfca2b2853a27bba2cf2ec
Tieda Wei <>
2020-02-09 00:43:18 -0500

    add demo project

--- Example/Lupi/ViewController.swift
+++ Example/Lupi/ViewController.swift
@@ -7,18 +7,51 @@
 //
 
 import UIKit
+import Lupi
 
 class ViewController: UIViewController {
-
+    
+    var videoBackgrounView: LupiVideoBackgroundView?
+    
+    let registerButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        videoBackgrounView = try? LupiVideoBackgroundView(fileName: "beach", in: view)
+        
+        layoutWelcomeLabel()
+        layoutRegisterButton()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    fileprivate func layoutWelcomeLabel() {
+        let welcomeLabel = UILabel()
+        welcomeLabel.text = "Welcome"
+        let font = UIFont(name: "AvenirNext-Bold", size: .init(60))!
+        welcomeLabel.font = font
+        welcomeLabel.textColor = .white
+        view.addSubview(welcomeLabel)
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            welcomeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100)
+        ])
     }
-
+    
+    fileprivate func layoutRegisterButton() {
+        view.addSubview(registerButton)
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        registerButton.setTitle("Register", for: .normal)
+        registerButton.setTitleColor(.white, for: .normal)
+        registerButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .heavy)
+        registerButton.backgroundColor = #colorLiteral(red: 0.8235294118, green: 0, blue: 0.3254901961, alpha: 1)
+        registerButton.layer.cornerRadius = 10
+        NSLayoutConstraint.activate([
+            registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            registerButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            registerButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            registerButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            registerButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
 }
-
